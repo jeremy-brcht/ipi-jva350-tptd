15dfed65a5f73528c2f7ba4bb5bcecb7958861b4
Jeremy Brochet <jejefish69@gmail.com>
2022-12-04 23:56:19 +0100

    testPartCongesPrisTotauxAnneeNMoins1

--- src/test/java/com/ipi/jva350/SalarieAideADomicileRepositoryTest.java
+++ src/test/java/com/ipi/jva350/SalarieAideADomicileRepositoryTest.java
@@ -3,6 +3,8 @@ package com.ipi.jva350;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -29,5 +31,24 @@ public class SalarieAideADomicileRepositoryTest {
         aide.setNom("jeremy");
         salarieAideADomicileRepository.save(aide);
         Assertions.assertThat(salarieAideADomicileRepository.findByNom("jeremy")).isNotNull();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 4, 0.5",
+            "4, 4, 1",
+            "3, 4, 0.75",
+
+    })
+    void testPartCongesPrisTotauxAnneeNMoins1(Double pris, Double acquis, Double res) {
+
+        SalarieAideADomicile aide = new SalarieAideADomicile();
+
+        aide.setCongesPayesPrisAnneeNMoins1(pris);
+        aide.setCongesPayesAcquisAnneeNMoins1(acquis);
+        
+        salarieAideADomicileRepository.save(aide);
+        
+        Assertions.assertThat(salarieAideADomicileRepository.partCongesPrisTotauxAnneeNMoins1()).isEqualTo(res);
     }
 }
