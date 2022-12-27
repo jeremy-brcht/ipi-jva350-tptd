98c43abf0dbb85dff3332060c6b45bc9dc60256c
Jeremy Brochet <jejefish69@gmail.com>
2022-12-27 21:39:26 +0100

    TU testEstJourFerie

--- src/test/java/com/ipi/jva350/EntrepriseTest.java
+++ src/test/java/com/ipi/jva350/EntrepriseTest.java
@@ -9,11 +9,18 @@ import org.junit.jupiter.params.provider.CsvSource;
 import com.ipi.jva350.model.Entreprise;
 
 public class EntrepriseTest {
-    
+
     @ParameterizedTest
     @CsvSource({
             "'2022-11-01', true",
-            "'2022-11-02', false",
+            "'2022-08-15', true",
+            "'2022-05-01', true",
+            "'2022-12-25', true",
+            "'2022-01-01', true",
+            "'2022-02-11', false",
+            "'2022-10-23', false",
+            "'2022-03-27', false",
+            "'2022-09-09', false",
 
     })
     void testEstJourFerie(LocalDate jour, Boolean expected) {
@@ -22,7 +29,7 @@ public class EntrepriseTest {
 
         Assertions.assertThat(res).isEqualTo(expected);
     }
-    
+
     @ParameterizedTest
     @CsvSource({
             "'2022-11-03', '2022-11-02', '2022-11-04', true",
